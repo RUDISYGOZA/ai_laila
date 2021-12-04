1fc26478b4607af8e4d0deb5339986382e803fbe
RUDISYGOZA <88480821+RUDISYGOZA@users.noreply.github.com>
2021-12-05 00:30:55 +0200

    upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    audio = r.listen()
+    audio = r.listen(source)
     
     text = r.recognize_google(audio)
     
